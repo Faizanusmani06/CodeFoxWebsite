a9ae5fcad9a8db48ccc04831b33aa2bea55d9b22
MOH FAIZAN <faizanusmani06@gmail.com>
2024-12-04 15:53:37 +0530

    minor errors

--- Backend/controller/mcq.ts
+++ Backend/controller/mcq.ts
@@ -15,7 +15,10 @@ const mcqController = (req: Request, res: Response) => {
   
         title : ${title}
       
-        channel: ${channel}`;
+        channel: ${channel}
+        
+        if you can't access please generate based on the topic
+        `;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
